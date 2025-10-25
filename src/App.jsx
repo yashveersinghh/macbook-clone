00b00da80ba1f825b44cbaf8dfbddfb6f3a42e4e
Yashveer singh <yashveersingh1921@gmail.com>
2025-10-25 21:41:52 +0530

    Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import Showcase from './components/Showcase';
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   )
 }
